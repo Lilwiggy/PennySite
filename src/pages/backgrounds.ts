a04cc0d027914432587be8d7b28e0ab539fb5bfd
Lilwiggy <lilwiggy123@gmail.com>
2021-03-02 22:00:30 -0600

    Change how the backend handles background fetching

---
+++ src/pages/backgrounds.ts
@@ -0,0 +1,17 @@
+import url from 'url';
+import { Request, Response } from 'express';
+import { items, ItemInfo } from './../modules/shop';
+exports.run = async (req: Request, res: Response) => {
+  // Fetch all backgrounds
+  let shopItems = Object.keys(items);
+  let bgs: ItemInfo[] = [];
+  shopItems.forEach((x) => {
+    if (items[x].type === 'background') bgs.push(items[x]);
+  });
+  return bgs;
+};
+
+exports.meta = {
+  name: 'shop/backgrounds',
+  type: 'get',
+};
